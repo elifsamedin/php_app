724619c0a4e3706629ea3d5d841ed18df03d0d8f
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-08 10:44:51 +0200

    version 1

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo 'httpd is running on '.$_SERVER['SERVER_NAME'].'<br>';
+echo ' with  ip '.$_SERVER['SERVER_ADDR'].'<br>';
+echo 'Current PHP version is: ' . phpversion().'<br><br>';
+echo 'APP Version 1'
+?>
