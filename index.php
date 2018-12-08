51df801494b967cc86a925a71b02c41ff4c1875d
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-08 10:49:15 +0200

    version 2

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo 'httpd is running on '.$_SERVER['SERVER_NAME'].'<br>';
 echo ' with  ip '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'Current PHP version is: ' . phpversion().'<br><br>';
-echo 'APP Version 1'
+echo 'APP Version 2'
 ?>
