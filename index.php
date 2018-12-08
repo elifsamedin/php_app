0e6b60060abd0e2d55f12fa211863aedb24a289f
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-08 11:08:53 +0200

    added server host

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 echo 'httpd is running on the server '.$_SERVER['HTTP_HOST'].'with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
 echo ' with  ip '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'Current PHP version is: ' . phpversion().'<br><br>';
-echo 'APP Version 1'
+echo 'APP Version 1';
+echo php_uname();
+
 ?>
 ~                    
