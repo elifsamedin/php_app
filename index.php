987f36364e46c9221d62040ca67d2e3c2053cb11
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-08 11:16:54 +0200

    cleaned up a bit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo 'httpd is running on the server '.php_uname(n).'with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
-echo ' with  ip '.$_SERVER['SERVER_ADDR'].'<br>';
-echo 'Current PHP version is: ' . phpversion().'<br><br>';
-echo 'APP Version 1';
+echo 'PHP appa is running on '.php_uname(n).'<br>with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
+echo 'IP'.$_SERVER['SERVER_ADDR'].'<br>';
+echo 'PHP version is: ' . phpversion().'<br><br>';
+echo 'Version 1';
 ?>
