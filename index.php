26eee251dafc1c1086e34beb2be18ccc38cd5e67
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-08 11:13:47 +0200

    using php_uname()

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-echo 'httpd is running on the server '.$_SERVER['HTTP_HOST'].'with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
+echo 'httpd is running on the server '.php_uname(n).'with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
 echo ' with  ip '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'Current PHP version is: ' . phpversion().'<br><br>';
 echo 'APP Version 1';
-echo php_uname();
-
 ?>
-~                    
