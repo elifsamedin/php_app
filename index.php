7fbbb9494ed5e67d41a4b4b918431e7982fa8389
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-15 17:39:22 +0200

    version 3

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo 'PHP appa is running on '.php_uname(n).'<br>with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
-echo 'IP'.$_SERVER['SERVER_ADDR'].'<br>';
+echo 'IP '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'PHP version is: ' . phpversion().'<br><br>';
-echo 'Version 2';
+echo 'Version 3';
 ?>
