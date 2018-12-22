9022144cfea81f3373d7f92e13227ae9223bff60
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-22 11:45:49 +0200

    updated version number for exercises

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo 'PHP appa is running on '.php_uname(n).'<br>with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
 echo 'IP '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'PHP version is: ' . phpversion().'<br><br>';
-echo 'Version 1';
+echo 'Version exercise 17';
 ?>
