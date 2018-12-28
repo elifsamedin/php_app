f7bbee72f8b0d5293e2625f4664478474c167492
Bogdan Benea <benea_bogdan@yahoo.com>
2018-12-28 16:15:19 +0200

    updated version number for exercises

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo 'PHP appa is running on '.php_uname(n).'<br>with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
+echo 'PHP app is running on '.php_uname(n).'<br>with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
 echo 'IP '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'PHP version is: ' . phpversion().'<br><br>';
-echo 'Version exercise 17';
+echo 'Version exercise 15 docke rimage';
 ?>
