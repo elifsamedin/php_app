a3812a413d85ece9932c25aea3d576b2b96a791c
Bogdan Benea <benea_bogdan@yahoo.com>
2019-01-12 07:34:32 +0200

    updated version number for exercises

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 echo 'PHP app is running on '.php_uname(n).'<br>with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
 echo 'IP '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'PHP version is: ' . phpversion().'<br><br>';
-echo 'Version exercise git mage 2 applications';
+echo 'Version exercise 18';
 ?>
