366b0305c50ca1e63483d73f3995cfe7e09289ce
root <root@ocmaster.lab.example.com>
2019-04-22 10:42:54 +0300

    Updated index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo 'App is running on '.php_uname(n).'<br>('.$_SERVER['SERVER_NAME'].')<br>';
-echo 'IP Address'.$_SERVER['SERVER_ADDR'].'<br>';
+echo 'App is running on '.php_uname(n).' ('.$_SERVER['SERVER_NAME'].')<br>';
+echo 'IP Address: '.$_SERVER['SERVER_ADDR'].'<br>';
 echo 'PHP Version: ' . phpversion().'<br><br>';
 ?>
