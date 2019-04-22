ef350703bd1eed92790fae7288b77cb5043bfdb0
root <root@ocmaster.lab.example.com>
2019-04-22 10:03:41 +0300

    Updated index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-echo 'PHP app is running on '.php_uname(n).'<br>with the virtual host '.$_SERVER['SERVER_NAME'].'<br>';
-echo 'IP '.$_SERVER['SERVER_ADDR'].'<br>';
-echo 'PHP version is: ' . phpversion().'<br><br>';
-echo 'Version exercise gitmage';
+echo 'App is running on '.php_uname(n).'<br>('.$_SERVER['SERVER_NAME'].')<br>';
+echo 'IP Address'.$_SERVER['SERVER_ADDR'].'<br>';
+echo 'PHP Version: ' . phpversion().'<br><br>';
 ?>
